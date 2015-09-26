2fb3fdd49f84028809582b4df61f41818663b6ca
nameofname <nameofemail@gmail.com>
2015-09-26 00:05:51 -0400

    modified find factorials logic

--- findFactorials.js
+++ findFactorials.js
@@ -1,5 +1,19 @@
-/**
- * User: ronald
- * Date: 9/25/15
- * Time: 11:39 PM
- */
+var factorials = {
+    0 : 1
+};
+var lastUsedNum = 0;
+
+var _nextFactorial = function () {
+    var last = lastUsedNum;
+    lastUsedNum ++;
+    factorials[lastUsedNum] = factorials[last] * lastUsedNum;
+};
+
+var findFactorial = function (nth) {
+    while (lastUsedNum <= nth) {
+        _nextFactorial();
+    }
+    return factorials[nth];
+};
+
+module.exports = findFactorial;
